b04971bc22dc70325f05f633b1ef9ec6e8b21b4b
jiyeon <jiyeon@218.38.137.27>
2018-09-17 21:49:26 +0900

    객체 비교 추

--- DriverInterfaceTest/src/com/jiyeon/DriverInterfaceTest/Driver.java
+++ DriverInterfaceTest/src/com/jiyeon/DriverInterfaceTest/Driver.java
@@ -3,6 +3,11 @@ package com.jiyeon.DriverInterfaceTest;
 public class Driver {
 
 	public void drive(Vehicle vehicle) {
+		
+		if (vehicle instanceof Bus) {
+			Bus bus = (Bus) vehicle;
+			bus.checkFare();
+		}
 		vehicle.run();
 	}
 }
